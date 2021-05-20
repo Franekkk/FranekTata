2715fbe295db2628dfc40b291dfacd8133ee8c17
Franek <kamieniecki.franek@gmail.com>
2021-05-20 17:25:44 +0200

    wyswitlanie wartosci inputow

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('Test');
+const form = document.querySelector('.formularz')
+
+
+function handleSubmit(event) {
+    event.preventDefault()
+
+    const firstValue = this.firstForm.value
+    const secondValue = this.secondForm.value
+    const thirdValue = this.thirdForm.value
+    console.log(firstValue, secondValue, thirdValue)
+
+}
+
+form.addEventListener('submit', handleSubmit)
